3717fda7dfe07721e1da7f6593ad53a6d30d3d63
GitWorkingTime <dlee28262@gmail.com>
2026-01-23 12:43:54 -0500

    Implemented displaying weather info

--- src/index.js
+++ src/index.js
@@ -1,24 +1,56 @@
-async function getWeatherData() {
-    const response = await fetch('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/[LOCATION]?key=[YOUR KEY]');
+import "./styles.css"
+
+async function getWeatherData(location) {
+    const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=EXPHN8WUDLHV87H4H2CT2W46C&unitGroup=metric`);
     const weatherData = await response.json();
-    console.log(weatherData);
-
-    for(let i = 0; i < weatherData.days.length; ++i ) {
-        let day = {
-            datetime: weatherData.days[i].datetime,
-            datetimeEpoch: weatherData.days[i].datetimeEpoch,
-            conditions: weatherData.days[i].conditions,
-            feelslike: weatherData.days[i].feelslike,
-            feelslikemin: weatherData.days[i].feelslikemin,
-            feelslikemax: weatherData.days[i].feelslikemax,
-            temp: weatherData.days[i].temp,
-            tempmax: weatherData.days[i].tempmax,
-            tempmin: weatherData.days[i].tempmin,
-            hours: weatherData.days[i].hours
-        }
-
-        console.log(day);
+    
+    let day = {
+        location: location,
+        datetime: weatherData.days[0].datetime,
+        datetimeEpoch: weatherData.days[0].datetimeEpoch,
+        conditions: weatherData.days[0].conditions,
+        feelslike: weatherData.days[0].feelslike,
+        feelslikemin: weatherData.days[0].feelslikemin,
+        feelslikemax: weatherData.days[0].feelslikemax,
+        temp: weatherData.days[0].temp,
+        tempmax: weatherData.days[0].tempmax,
+        tempmin: weatherData.days[0].tempmin,
+        hours: weatherData.days[0].hours
     }
+
+
+    updateClient(day);
 }
 
-getWeatherData();
+const form = document.querySelector("form");
+form.addEventListener("submit", (evt) => {
+    evt.preventDefault();
+
+    const locationInput = document.querySelector("#location-search");
+    let location = locationInput.value.toLowerCase();
+
+    console.log(location);
+    locationInput.value = "";
+    getWeatherData(location);
+});
+
+// DOM Manipulation
+const overlay = document.querySelector("#overlay");
+const location = document.querySelector("#location");
+const temp = document.querySelector(".main>.temp");
+const tempMin = document.querySelector(".temp-min");
+const tempMax = document.querySelector(".temp-max");
+const condition = document.querySelector("#condition");
+
+function updateClient(dayObj) {
+    location.textContent = dayObj.location;
+    temp.textContent = `${dayObj.temp}℃`;
+    tempMin.textContent = `Min: ${dayObj.tempmin}℃`;
+    tempMax.textContent = `Max: ${dayObj.tempmax}℃`;
+    condition.textContent = `${dayObj.conditions}`;
+}
+
+
+
+
+
